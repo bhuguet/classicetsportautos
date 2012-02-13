4fe30d7afcab86e69a88a6217609a000e63567d0
classicetsportautos@gmail.com <classicetsportautos@gmail.com@27e19835-539d-85bd-6db8-362e94f4f2e1>
2012-02-13 23:27:47 +0000

    Set image resizer on the fly
    git-svn-id: https://classicetsportautos.googlecode.com/svn/trunk@80 27e19835-539d-85bd-6db8-362e94f4f2e1

---  classicetsportautos --username classicetsportautos@gmail.com/classes/Car.php
+++  classicetsportautos --username classicetsportautos@gmail.com/classes/Car.php
@@ -5,10 +5,11 @@ class Car {
     var $label;
     var $imageName;
     var $cssClass;
+    var $otherPictures;
     const BIG_NAME_SUFFIXE = "_b.jpg";
     const SMALL_NAME_SUFFIXE = "_s.jpg";
     
-    public function __construct($imageName, $label, $cssClass) {
+    public function __construct($imageName = "", $label, $cssClass = "") {
         $this->label = $label;
         $this->imageName = $imageName;
         $this->cssClass = $cssClass;
@@ -22,12 +23,32 @@ class Car {
         return $path . $this->imageName . self::SMALL_NAME_SUFFIXE;
     }
     
+    public function getImage($path) {
+        return $path . $this->imageName;
+    }
+    
+    public function getResizedImage($path, $size) {
+        return $this->getResizedImageForPicture($path, $size, $this->imageName);
+    }
+ 
+    public function getResizedImageForPicture($path, $size, $pictureName) {
+        return "/utils/image.php/".$pictureName."?height=".$size."&width=".$size."&cropratio=1:1&image=".$path.$pictureName;
+    }    
+    
     public function getLabel() {
         return $this->label;
     }
     
     public function getCssClass() {
         return $this->cssClass;
+    }
+    
+    public function setOtherPictures($otherPictures = array()) {
+        $this->otherPictures = $otherPictures;
+    }
+    
+    public function getOtherPictures() {
+        return $this->otherPictures;
     }
 }
 
