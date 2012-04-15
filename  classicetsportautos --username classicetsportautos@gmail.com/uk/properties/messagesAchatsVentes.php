602d827673fba7ef4cfd7a8dd5f7885c9f11d5fc
classicetsportautos@gmail.com <classicetsportautos@gmail.com@27e19835-539d-85bd-6db8-362e94f4f2e1>
2012-04-15 14:20:59 +0000

    version anglaise du fichier
    git-svn-id: https://classicetsportautos.googlecode.com/svn/trunk@97 27e19835-539d-85bd-6db8-362e94f4f2e1

---  classicetsportautos --username classicetsportautos@gmail.com/uk/properties/messagesAchatsVentes.php
+++  classicetsportautos --username classicetsportautos@gmail.com/uk/properties/messagesAchatsVentes.php
@@ -1,49 +1,35 @@
 <?php
 
-$achats_ventes_description="Vendez ou recherchez votre voiture ancienne grâce à Classic et Sport Autos";
-$achats_ventes_keywords="vente, achat, ancienne, annonce";
-$achats_ventes_title="Classic & Sport Autos - Annonces pour l'Achat ou la Vente de voitures anciennes";
+$achats_ventes_description="Sell or buy your classic car through Classic & Sport Autos";
+$achats_ventes_keywords="Sell, Buy, classic, advert";
+$achats_ventes_title="Classic & Sport Autos – Classic cars sell or buy adverts";
 
-$annonces_title="Annonces";
-$annonces_description="Vous recherchez une voiture ancienne ou désirez vendre la votre ? Ecrivez à <a href='mailto:contact@classicetsportautos.fr'>contact@classicetsportautos.fr</a> ou appelez le +33(0)4.57.93.38.57.";
+$annonces_title="Adverts";
+$annonces_description="You are looking for a classic car or you would like to sell yours, write to<a href='mailto:contact@classicetsportautos.fr'>contact@classicetsportautos.fr</a> or call +33(0)4.57.93.38.57.";
 
-$vente_title="Vente";
-$prix="Prix:";
-$ville="Ville:";
-$annee_modele="Année-modèle:";
-$date_publication="Publié le";
-$kilometrage="Kilométrage:";
+$vente_title="Sell";
+$prix="Price:";
+$ville="City:";
+$annee_modele="Year";
+$date_publication="published on";
+$kilometrage="Mileage:";
 
-$frogeye_description="Magnifique Austin Healey Frogeye de 1960 entierement restaurée (Provenance Desurmont), freins à disque à l'avant.";
-$tr5_description="Très belle TR5 PI de 1968, restaurée en 1995.";
-$corvette_description="Belle Chevrolet Corvette de 1958. Très bon état. Pont changé.";
-$xk120Roadster_description_1="Belle Jaguar XK 120 roadster de 1953, conduite à droite d'origine, de couleur rouge avec de jolis sièges bi-ton.";
-$xk120Roadster_description_2="Quelques modifications discrètes mais apportant plus de confort: ventilateur électrique, embrayage à 
-                             commnande hydraulique, amortisseurs télescopiques à l'arrière.";
-$daimler_description_1="Jolie Daimler Sovereign 4,2l de 1980. 93000km au compteur, même propriétaire depuis 1994 ayant acheté 
-                       la voiture à 48303km.";
-$daimler_description_2="Nombreux travaux dont moteur refait à 86000km, crémaillère direction à 77600km, 
-                       pompe à eau à 68200, boite de vitesse à 67400 et peinture à 65000km (1997).</p>
-                       <p>Pneus Michelin XWX bon état.";
-$sl190_description="Mercedes 190SL cabriolet blanche, intérieur cuir rouge, avec hardtop, en très bon état.</br>
-                    Belle peinture avec quelques petits défauts. 
-                    Dessous propre sans corrosion, moteur fiable, très bonne pression d'huile.
-                    Bel intérieur en cuir rouge.
-                    Capote alpaga comme neuve. ";
-$dynolite_apg_30_desc="Huile spécifique Multigrade 25w/70 remplaçant les Monogrades SAE40. Cette viscosité est spécialement
-                      adaptée pour les moteurs d'après-guerre de forte cylindrées ou pour les moteur possédant une pompe à
-                      huile de faible débit, ainsi que pour la majorité des Motos d'après guerre.
-                      Cette huile contient des additifs spéciaux qui de par leurs particularités amélioreront la pression à
-                      chaud ainsi que la consommation d'huile.";
-$dynolite_hypoid_desc="Cette huile multigrade remplace la EP90 et dépasse la Norme «GL5». Elle est à utiliser dans la plupart
-                       des ponts Hypoïdes d'après 1960.";
-$dynolite_gearoil_40_desc="Cette huile multigrade est à utiliser en remplacement des SAE40 et EP90. Elle est
-			  compatibles avec les boîtes à taille droite, synchrones et à pignons hypocycloïdes tels que les overdrives.
-			  Elles ne contient aucun additif de type «EP» qui détruit les métaux tels que le Bronze le Cuivre et le
-			  Laiton. Cette huile ne contient aucun additif d'antifriction moléculaire à effet de surface provoquant
-			  le patinage des embrayages d'overdrive et des boîtes à présélection (Cotal, Wilson).";
+$frogeye_description="Beautiful 1960 Austin Healey Frogeye fully restored (Desurmont origin), front brake disks.";
+$tr5_description="Very nice 1968 Triumph TR5 PI, completely restored in 1995.";
+$corvette_description="Great 1958 Chevrolet Corvette de 1958. Runs very well. Rebuilt differential.";
+$xk120Roadster_description_1="Very nice 1953 Jaguar XK 120 roadster, original RHD, red with nice dual tone seats.";
+$xk120Roadster_description_2="A few discrete improvements bringing more comfort, electric cooling fan, hydraulic clutch, rear telescopic dampers.";
+$daimler_description_1="Nice 1980 4.2l Daimler Sovereign. 93000km on the odometer, current owner having bought the car in 1994 with 48303km on the odometer.";
+$daimler_description_2="Major maintenance expenses include engine rebuilt at 86000km, steering rack at 77600km, water pump at 68200km, gearbox at 67400km, new paint at 65000km(1997).</p>
+                       <p> Good condition XWX Michelin.";
+$sl190_description="1961 Mercedes 190SL roadster, red leather interior, hardtop, very good condition.</br>
+                    Nice paint with few chips.
+                    Reliable engine, very good oil pressure.
+                    Mohair hood as new. ";
+$dynolite_apg_30_desc="Mineral Multigrade 20w/60 oil replacing SAE30 oil. This viscosity is perfect for xxxxxxx.";
+$dynolite_hypoid_desc="This oil replaces EP90 grade oil and exceeds GL5 norm. It is used in most post 1960 hypoid differentials.";
+$dynolite_gearoil_40_desc="This oil is used in gearboxes in replacement of SAE 40 and EP90. It is compatible with straight cut gears gearboxes, yyyy and hypocycloidal gears as in overdrives. It does not contain any EP additive which could damage bronze, copper or brass. This oil does not contain antifriction additives with molecular surface effects inducing slippage in overdrives and preselection gearboxes (Cotal, Wilson).";
 
-$cherche_tr3="Cherche Triumph TR3 à rénover.
-             Prix maximum: 7000€;
-             Etat épave accepté.
-             Toutes régions.";
+$cherche_tr3="Looking for Triumph TR3 or MGA to restore.
+             Maximum price: 10000€;
+             Any region.";
