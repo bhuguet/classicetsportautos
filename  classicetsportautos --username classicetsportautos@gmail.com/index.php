0064e6d11e98ca37d9d574942c50475d0132eac2
classicetsportautos@gmail.com <classicetsportautos@gmail.com@27e19835-539d-85bd-6db8-362e94f4f2e1>
2013-06-19 12:47:07 +0000

    set show faces to true only way found to activate stream display (had disapeared few days ago)
    git-svn-id: https://classicetsportautos.googlecode.com/svn/trunk@132 27e19835-539d-85bd-6db8-362e94f4f2e1

---  classicetsportautos --username classicetsportautos@gmail.com/index.php
+++  classicetsportautos --username classicetsportautos@gmail.com/index.php
@@ -53,7 +53,7 @@
 			<div>
                             <div id="fb-root"></div>
                             <script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
-                            <fb:like-box href="https://www.facebook.com/pages/Classic-Sport-Autos/124036417669507" width="560" show_faces="false" stream="true" header="false"></fb:like-box>
+                            <fb:like-box href="https://www.facebook.com/pages/Classic-Sport-Autos/124036417669507" width="560" show_faces="true" stream="true" header="false"></fb:like-box>
 			</div>
 
 			<div class="clearit"></div>
